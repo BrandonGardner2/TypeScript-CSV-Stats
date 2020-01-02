ac2fbfef40ee70239e7971c61705ed843424e4e9
Brandon Gardner <brandon.gardner2@outlook.com>
2020-01-02 11:38:57 -0600

    basic analysis using an enum

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,20 @@ const matches = fs.readFileSync('football.csv', {
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
+
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manUnitedWins += 1;
+  else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manUnitedWins + 1;
+}
+
+console.log(manUnitedWins);
+
