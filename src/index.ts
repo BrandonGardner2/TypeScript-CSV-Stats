de47c72ee82381f8a27657a117dd01cf075b0a06
Brandon Gardner <brandon.gardner2@outlook.com>
2020-01-02 13:38:42 -0600

    implements everything together to make a report

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import { Summary } from './models/Summary';
+import { WinsAnalysis } from './models/WinsAnalysis';
+import { ConsoleReport } from './models/ConsoleReport';
 import { CSVReader } from './models/CSVReader';
 import { MatchReader } from './models/MatchReader';
-import { MatchResult } from './types/MatchResult';
 
 const csvFileReader = new CSVReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-const { matches } = matchReader;
 
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport(),
+);
+
+summary.buildAndReport(matchReader.matches);
